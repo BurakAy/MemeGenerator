a22d79554fc655ca3e2c21381a539a26156c4bf4
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-31 08:46:39 -0600

    added method for gettting image on button click

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,16 @@
 import "../styles/Meme.css";
+import memesData from "./memesData";
+import { useState } from "react";
 
 const Meme = () => {
+  const [memeImage, setMemeImage] = useState("");
+
+  const getMemeImage = () => {
+    const memes = memesData.data.memes;
+    const randMeme = Math.floor(Math.random() * memes.length);
+    setMemeImage(memes[randMeme].url);
+  };
+
   return (
     <section className="meme--form_container">
       <div className="meme--form">
@@ -8,9 +18,13 @@ const Meme = () => {
           <input type="text" placeholder="Shut up" />
           <input type="text" placeholder="and take my money" />
         </div>
-        <button className="meme--button" type="submit">
+        <button className="meme--button" type="submit" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
+      </div>
+
+      <div className="meme--image_container">
+        <img src={memeImage} />
       </div>
     </section>
   );
